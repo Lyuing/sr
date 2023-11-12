acf28683761fd397067ffdda6b4c4d2f4ec6001b
undefined <18868801547@163.com>
2023-11-12 21:57:14 +0800

    图片替换，增加二维码

--- js/pictures.js
+++ js/pictures.js
@@ -2,6 +2,8 @@
 const bannersDto = [
   {src: 'https://pic.imgdb.cn/item/654f6e77c458853aef12bf5e.jpg'},
   {src: 'https://pic.imgdb.cn/item/654f6e77c458853aef12bfdf.jpg'},
+  {src: 'https://pic.imgdb.cn/item/6550cf8cc458853aef726c9c.jpg'},
+  {src: 'https://pic.imgdb.cn/item/6550cf8dc458853aef727410.jpg'},
 ]
 const visitsDto = [
   'https://pic.imgdb.cn/item/654f847bc458853aef75a38f.jpg',
@@ -12,27 +14,27 @@ const visitsDto = [
 ]
 const dto = [
   {
-    title: 'Titanium Plate （钛板）',
+    title: 'Titanium Plate/Sheet',
     description: 'High Quantity GR5 GR7 Titanium Plates And Sheets Titanium Alloy Factory Supply Hot Sale.',
     pictures: [
       { src: 'https://pic.imgdb.cn/item/654f7fb4c458853aef5d6558.jpg' },
       { src: 'https://pic.imgdb.cn/item/654f80cdc458853aef63198e.jpg' },
+      { src: 'https://pic.imgdb.cn/item/6550c0f9c458853aef3347eb.jpg' },
       { src: 'https://pic.imgdb.cn/item/654f811dc458853aef64a849.jpg' },
-      { src: 'https://pic.imgdb.cn/item/654f8142c458853aef655e47.jpg' },
     ],
   },
   {
-    title: 'Titanium Bar/Rod （钛棒）',
+    title: 'Titanium Bar/Rod',
     description: 'Wholesale ASTM F1713 TI13NB13ZR Titanium Bar Medical Grade 2 5MM Titanium ROD.',
     pictures: [
       { src: 'https://pic.imgdb.cn/item/654f81e0c458853aef686913.jpg' },
       { src: 'https://pic.imgdb.cn/item/654f81ffc458853aef690a84.jpg' },
-      // { src: 'https://pic.imgdb.cn/item/653e042ac458853aeff5109e.jpg' },
-      // { src: 'https://pic.imgdb.cn/item/653e043dc458853aeff58550.jpg' },
+      { src: 'https://pic.imgdb.cn/item/6550bf10c458853aef2ae630.png' },
+      { src: 'https://pic.imgdb.cn/item/6550bfd8c458853aef2e1a14.jpg' },
     ],
   },
   {
-    title: 'Titanium Tube （钛管）',
+    title: 'Titanium Tube',
     description: 'ASTM B338 B16 Grade 2 Seamless Titanium Tube.',
     pictures: [
       { src: 'https://pic.imgdb.cn/item/654f8234c458853aef6a1a6b.jpg' },
@@ -43,28 +45,7 @@ const dto = [
     ],
   },
   {
-    title: 'Titanium Flange （钛法兰）',
-    description: 'ANSI B16.5 DIN 150L BS Forging Carbon Steel Pipe Class 150 RF,FF,RTJ Weld Neck Flanges.',
-    pictures: [
-      { src: 'https://pic.imgdb.cn/item/654f82e4c458853aef6e64a6.jpg' },
-      { src: 'https://pic.imgdb.cn/item/65464867c458853aefaeb25c.jpg' },
-      { src: 'https://pic.imgdb.cn/item/65464867c458853aefaeb2f5.jpg' },
-      { src: 'https://pic.imgdb.cn/item/65464867c458853aefaeb3a1.jpg' },
-     ,
-    ],
-  },
-  {
-    title: 'Titanium Wire （钛丝）',
-    description: 'GR1 GR2 The Standard ASTM B863 Titanium Wire.',
-    pictures: [
-      { src: 'https://pic.imgdb.cn/item/654f838bc458853aef71691d.jpg' },
-      { src: 'https://pic.imgdb.cn/item/654f83a5c458853aef71e24f.jpg' },
-      { src: 'https://pic.imgdb.cn/item/654f83b6c458853aef722c49.jpg' },
-      { src:'https://pic.imgdb.cn/item/654f83edc458853aef732335.jpg'}
-    ]
-  },
-  {
-    title: 'Titanium Fasteners （钛紧固件）',
+    title: 'Titanium Fasteners',
     description: 'M14-1.5*28mm Titanium Alloy Car Wheel Bolts Audi Volkswagen Series Car Accessories Factory Supply Hot Sale.',
     pictures: [
       { src: 'https://pic.imgdb.cn/item/654f844ac458853aef74ba1a.jpg' },
@@ -74,7 +55,7 @@ const dto = [
     ],
   },
   {
-    title: 'Titanium Parts CNC Machining Service （定制加工件）',
+    title: 'Titanium Parts CNC Machining Service ',
     description: 'Parts Machining CNC Custom Machined Turning Supplier Milling Spare Part Factory Plastic Metal Steel Brass Aluminum CNC Service.',
     pictures: [
       { src: 'https://pic.imgdb.cn/item/654f8518c458853aef78d246.jpg' },
@@ -82,6 +63,27 @@ const dto = [
       { src: 'https://pic.imgdb.cn/item/654f8550c458853aef79c1fb.jpg' },
       { src: 'https://pic.imgdb.cn/item/654f8518c458853aef78d246.jpg' },
     ]
+  },
+  {
+    title: 'Titanium Wire',
+    description: 'GR1 GR2 The Standard ASTM B863 Titanium Wire.',
+    pictures: [
+      { src: 'https://pic.imgdb.cn/item/654f838bc458853aef71691d.jpg' },
+      { src: 'https://pic.imgdb.cn/item/654f83a5c458853aef71e24f.jpg' },
+      { src: 'https://pic.imgdb.cn/item/654f83b6c458853aef722c49.jpg' },
+      { src: 'https://pic.imgdb.cn/item/654f83edc458853aef732335.jpg' }
+    ]
+  },
+  {
+    title: 'Titanium Flange',
+    description: 'ANSI B16.5 DIN 150L BS Forging Carbon Steel Pipe Class 150 RF,FF,RTJ Weld Neck Flanges.',
+    pictures: [
+      { src: 'https://pic.imgdb.cn/item/654f82e4c458853aef6e64a6.jpg' },
+      { src: 'https://pic.imgdb.cn/item/65464867c458853aefaeb25c.jpg' },
+      { src: 'https://pic.imgdb.cn/item/65464867c458853aefaeb2f5.jpg' },
+      { src: 'https://pic.imgdb.cn/item/65464867c458853aefaeb3a1.jpg' },
+     ,
+    ],
   },
 ]
 
